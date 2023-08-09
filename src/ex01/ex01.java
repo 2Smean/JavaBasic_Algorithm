94bfbc40da97eef5f68ad77bd313bf4abef0f657
2Smean <136636643+2Smean@users.noreply.github.com>
2023-08-09 22:56:16 +0900

    123
    213

--- src/ex01/ex01.java
+++ src/ex01/ex01.java
@@ -6,6 +6,6 @@ package ex01;
 public class ex01 {
     public static void main(String[] args) {
         int n = 3;
-        System.out.println("n = " + n);
+        System.out.println("n = 1" + n);
     }
 }

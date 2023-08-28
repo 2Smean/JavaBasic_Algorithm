ec6a4cb5e657e62f7449309d68c67d8b9796b89c
2Smean <136636643+2Smean@users.noreply.github.com>
2023-08-28 09:35:51 +0900

    Hello~ Korea
    hi~

--- src/ex04/Strawberry.java
+++ src/ex04/Strawberry.java
@@ -1,12 +1,16 @@
 package ex04;
 
 public class Strawberry {
+    public static void main(String[] args) {
+
+
         String price;   // price와 taste는 인스턴스 변수
         String taste;   // 딸기의 가격과 맛은 각기 다르다.
-    static String water; // water는 클래스 변수, 딸기는 모두 물을 받으며 자란다.
-        static String growth; // growth는 클래스
-    // 변수 딸기는 모두 밭에서 자라지 않으므로 필요에 따라 이 값을 변경해 다른 값 출력
+        String water; // water는 클래스 변수, 딸기는 모두 물을 받으며 자란다.
+        String growth; // growth는 클래스
+        // 변수 딸기는 모두 밭에서 자라지 않으므로 필요에 따라 이 값을 변경해 다른 값 출력
 
+    }
 
 
 }
---
+++ src/ex04/연습.java
@@ -0,0 +1,12 @@
+package ex04;
+
+public class 연습 {
+
+    public static void main(String[] args) {
+        String  s1;
+        s1 = "Hello~";
+        String s2 = "Korea";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+    }
+}

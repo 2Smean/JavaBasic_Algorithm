61b693898d69cdc10ae024788d71c2dc714c11ed
2Smean <136636643+2Smean@users.noreply.github.com>
2023-09-10 20:05:00 +0900

    황 기 태 Scanner

--- src/Thisjava/Ex1_Example.java
+++ src/Thisjava/Ex1_Example.java
@@ -1,21 +1,30 @@
 package Thisjava;
-
 import java.util.Scanner;
-
 public class Ex1_Example {
-    public static void main(String[] args) {
-        byte n=125;
-        for(int i=0;i<5;i++) {
-            System.out.println(n);
+    public static void main(String args[]) {
+        System.out.println("이름, 도시, 나이, 체중, 독신여부를 입력하세요");
+        Scanner sc=new Scanner(System.in);
+            String name=sc.next();
+            String city=sc.next();
+            int age=sc.nextInt();
+            double weight=sc.nextDouble();
+            boolean isSingle=sc.nextBoolean();
 
-            System.out.println(n);
+            System.out.print("이름은"+name+",");
+            System.out.print("사는 도시는"+city+",");
+            System.out.print("나이는"+age+",");
+            System.out.print("체중은"+weight+",");
+            System.out.println("독신여부는"+isSingle);
+            
+
+
+
+
+
         }
 
+
+
+
     }
-
-
-
-
-}
-
 

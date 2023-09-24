454e9642354371f6cdf449ec7253826bcc4fac4f
2Smean <136636643+2Smean@users.noreply.github.com>
2023-09-24 14:55:57 +0900

    예제 4-7 : 객체 배열 만들기 연습
    몰입, 황농문

---
+++ src/CircleArray/BookArray.java
@@ -0,0 +1,30 @@
+//Book 클래스를 활용하여 2개짜리 Book 객체 배열을 만들고,
+//사용자로 부터 책의 제목 title과 저자 author를 입력 받아 배열을 완성하라
+package CircleArray;
+
+import java.util.Scanner;
+
+class Book {
+    String title, author;
+public Book(String title, String author) {
+    this.title = title;
+    this.author = author;
+}
+}
+
+
+public class BookArray {
+    public static void main(String[] args) {
+        Book [] book = new Book[2]; //Book 배열 선언
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<book.length; i++) {
+            System.out.println("제목을 입력");
+            String title = sc.nextLine();
+            System.out.println("작가를 입력");
+            String author = sc.nextLine();
+            book[i] = new Book(title, author); // 배열 원소 객체 생성
+        }
+        for(int i=0; i<book.length; i++)
+            System.out.println("(" + book[i].title + "," + book[i].author + ")");
+    }
+}
---
+++ src/CircleArray/CircleArray.java
@@ -0,0 +1,33 @@
+package CircleArray;
+
+import java.util.Scanner;
+
+class Circle {
+    int radius;
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return 3.14*radius*radius;
+    }
+}
+
+public class CircleArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // 사용자에게 Circle 의 반지름을 입력받겠다
+        Circle[] c; // Circle 배열에 대한 레퍼런스 변수 c 선언
+        c = new Circle [5]; // 레퍼런스 배열을 생성
+        for (int i = 0; i < c.length; i++) {
+            System.out.print((i+1) + ">>");
+            int radius = sc.nextInt();
+            c[i] = new Circle(radius); //0~4까지는 이상하니까 1을 더해서 1~5까지로 인덱스 값에 1을 더해줌
+        }
+        //생성 과정 끝
+
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i].getArea() + " ");
+        }
+    }
+
+}

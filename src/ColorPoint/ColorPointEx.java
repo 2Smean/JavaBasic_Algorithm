8c1d8760925c92cc97cbcadb1a66d77d76536845
2Smean <136636643+2Smean@users.noreply.github.com>
2023-10-05 18:37:03 +0900

    클래스 상속과 객체

---
+++ src/ColorPoint/ColorPointEx.java
@@ -0,0 +1,43 @@
+package ColorPoint;
+
+class Point{
+    private int x, y;
+
+    public void set(int x, int y) {
+        this.x = x; this.y = y;
+    }
+
+    public void showPoint() {
+        System.out.println("("+ x + "," + y + ")");
+    }
+}
+
+class ColorPoint extends Point{
+    private String color;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void showColorPoint() {
+        System.out.print(color);
+//      System.out.println(x);//private 이므로 오류발생
+        showPoint();
+
+    }
+}
+public class ColorPointEx {
+
+    public static void main(String[] args) {
+        Point p = new Point();
+        p.set(1,2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3,4);
+        cp.setColor("red");
+        cp.showPoint();//점만 찍는 것임
+        cp.showColorPoint();
+    }
+
+}
--- src/Thisjava/CalcEx.java
+++ src/Thisjava/CalcEx.java
@@ -2,8 +2,8 @@ package Thisjava;
 
 class Calc {
     public static int abs(int a) {return a>0?a:-a;}
-    public static int max(int a, int b) {return Math.max(a, b);}
-    public static int min(int a, int b) {return Math.min(a, b);}
+    public static int max(int a, int b) {return (a>b)?a:b;}
+    public static int min(int a, int b) {return (a>b)?b:a;}
 }
 
 public class CalcEx {

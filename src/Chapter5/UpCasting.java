ce5151b5b4f15cd7acc848e9af314a13a59586a0
2Smean <136636643+2Smean@users.noreply.github.com>
2023-10-07 11:13:37 +0900

    UpCasting

---
+++ src/Chapter5/UpCasting.java
@@ -0,0 +1,30 @@
+package Chapter5;
+class Person {
+    String name;
+    String id;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+class Student extends Person {
+    String grade;
+    String department;
+
+    public Student(String name) {
+        super(name);
+    }
+}
+
+public class UpCasting {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Person p;
+        Student s = new Student("이승민");
+        p = s;
+
+        System.out.println(p.name);
+    }
+
+}
--- src/ColorPoint/ColorPointEx.java
+++ src/ColorPoint/ColorPointEx.java
@@ -3,7 +3,7 @@ package ColorPoint;
 class Point{
     private int x, y;
 
-    public void set(int x, int y) {
+    public Point(int x, int y){
         this.x = x; this.y = y;
     }
 
@@ -15,7 +15,8 @@ class Point{
 class ColorPoint extends Point{
     private String color;
 
-    public void setColor(String color) {
+    public ColorPoint(int x, int y, String color){
+        super(x, y);
         this.color = color;
     }
 
@@ -29,13 +30,11 @@ class ColorPoint extends Point{
 public class ColorPointEx {
 
     public static void main(String[] args) {
-        Point p = new Point();
-        p.set(1,2);
+        Point p = new Point(1,2);
+//      p.set(1,2);// Point를 만들때 처음부터 1,2인 점을 만드는 것이 좋다
         p.showPoint();
 
-        ColorPoint cp = new ColorPoint();
-        cp.set(3,4);
-        cp.setColor("red");
+        ColorPoint cp = new ColorPoint(1,2, "red");
         cp.showPoint();//점만 찍는 것임
         cp.showColorPoint();
     }

bc9768ef9bf2b0d4442db2db38205adfb00e13b1
2Smean <136636643+2Smean@users.noreply.github.com>
2024-01-04 18:22:23 +0900

    Switch 문 break를 하지 않은 예시

---
+++ src/InflearnJava/cond/Switch3.java
@@ -0,0 +1,21 @@
+package InflearnJava.cond;
+/*2등급도 3등급과 같이 3000원 쿠폰을 준다*/
+public class Switch3 {
+    public static void main(String[] args) {
+        int grade = 3;
+
+        int coupon;
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+                break;
+            case 2:
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰" + coupon);
+    }
+}

4f8678be8daa4009d3841a70eda3782e9fab31f9
2Smean <136636643+2Smean@users.noreply.github.com>
2024-01-30 21:34:31 +0900

    김영한 자바 입문/연습문제 20240130 21:34

--- src/InflearnJava/Intro_Q21.java
+++ src/InflearnJava/Intro_Q21.java
@@ -13,7 +13,7 @@ public class Intro_Q21 {
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("**");
+                System.out.print("*");
             }
             System.out.println();
         }

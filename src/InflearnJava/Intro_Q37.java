90df192199235a36c5c0505c1d2827a78874a1a6
2Smean <136636643+2Smean@users.noreply.github.com>
2024-06-09 11:25:08 +0900

    commit 20240609

---
+++ src/InflearnJava/Intro_Q37.java
@@ -0,0 +1,27 @@
+package InflearnJava;
+
+import java.util.Scanner;
+
+public class Intro_Q37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] scores = new int[4][3];
+        String[] subjects = {"영어", "국어", "수학"};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println((i + 1) + "번 학생의 성적을 입력하세요 :");
+            for (int j = 0; j < subjects.length; j++) {
+                scores[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int total = 0;
+            for (int j = 0; j < subjects.length; j++) {
+                total += scores[i][j];
+            }
+            double average = total / 3.0;
+            System.out.println((i + 1) + "번 학생의 총점: " + total + ", 평균: " + average);
+        }
+    }
+}
---
+++ src/InflearnJava/Intro_Q39.java
@@ -0,0 +1,15 @@
+package InflearnJava;
+/*평균값 리펙토링*/
+public class Intro_Q39 {
+    public static void main(String[] args) {
+
+        System.out.println("평균값: " + average(1,2,3));
+
+        System.out.println("평균값: " + average(15,25,35));
+    }
+
+    public static double average(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum / 3.0 ;
+    }
+}

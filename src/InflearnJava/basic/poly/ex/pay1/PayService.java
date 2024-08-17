7fa98c6efc95c6d5a1cb5253e44852e762bf0ecb
2Smean <136636643+2Smean@users.noreply.github.com>
2024-08-17 15:49:17 +0900

    commit 20240817 inflearn 김영한 basic finish

--- src/InflearnJava/basic/poly/ex/pay1/PayService.java
+++ src/InflearnJava/basic/poly/ex/pay1/PayService.java
@@ -4,21 +4,10 @@ public class PayService {
 
     public void processPay(String option, int amount) {
 
-        boolean result;
         System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
 
-        Pay pay;
-        if (option.equals("kakao")) {
-            pay = new KakaoPay();
-            result = pay.pay(amount);
-        } else if (option.equals("naver")) {
-            pay = new NaverPay();
-            result = pay.pay(amount);
-        } else {
-            pay = null;
-            System.out.println("결제 수단이 없습니다");
-            result = false;
-        }
+        Pay pay = PayStore.findPay(option);//찾는 것
+        boolean result = pay.pay(amount);
 
         if (result) {
             System.out.println("결제가 성공했습니다");
